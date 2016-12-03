c668b5b1102082f2ab28688a5f051f077a90bfee
Jan Równicki <codeandsolder@gmail.com>
2016-12-03 16:50:34 +0100

    Finalizing multiple channel support;
    +minor fixes

--- OvenSoft/.ino.cpp
+++ OvenSoft/.ino.cpp
@@ -2,7 +2,7 @@
 //This is a automatic generated file
 //Please do not modify this file
 //If you touch this file your change will be overwritten during the next build
-//This file has been generated on 2016-12-03 14:42:42
+//This file has been generated on 2016-12-03 16:59:49
 
 #include "Arduino.h"
 #include <pins_arduino.h>
@@ -10,7 +10,6 @@
 #include <avr/pgmspace.h>
 void zeroCrossing() ;
 void setup() ;
-void nextNode() ;
 void updatePID() ;
 void loop() ;
 
